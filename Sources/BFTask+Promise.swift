15cda7d860d5f438395c0f31fcf868a0c9f0193d
Max Howell <mxcl@me.com>
2018-02-17 14:26:43 -0500

    Facebook broke the API between 1.8 and 1.9

--- Sources/BFTask+Promise.swift
+++ Sources/BFTask+Promise.swift
@@ -7,11 +7,11 @@ extension Promise {
     /**
      The provided closure is executed when this promise is resolved.
      */
-    public func then<U: AnyObject>(on q: DispatchQueue = conf.Q.map, body: @escaping (T) -> BFTask<U>) -> Promise<U?> {
+    public func then<U>(on q: DispatchQueue = conf.Q.map, body: @escaping (T) -> BFTask<U>) -> Promise<U?> {
         return then(on: q) { tee -> Promise<U?> in
             let task = body(tee)
             return Promise<U?> { seal in
-                task.continue({ task in
+                task.continueWith(block: { task in
                     if task.isCompleted {
                         seal.fulfill(task.result)
                     } else if let error = task.error {
